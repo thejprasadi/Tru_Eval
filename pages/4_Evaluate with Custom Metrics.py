a730359a1a3f9e92b4fbab7770b653e0c2c059c2
thejprasadi <34730639+thejprasadi@users.noreply.github.com>
2024-05-22 17:28:31 +0530

    Update 4_Evaluate with Custom Metrics.py

--- pages/4_Evaluate with Custom Metrics.py
+++ pages/4_Evaluate with Custom Metrics.py
@@ -277,8 +277,24 @@ if submitted_btn:
     st.write("")
     
     
-    for feedback, feedback_result in rec.wait_for_feedback_results().items():
-        st.write(feedback.name, feedback_result.result)
+    # for feedback, feedback_result in rec.wait_for_feedback_results().items():
+    #     st.write(feedback.name, feedback_result.result)
+    for feedback, feedback_result in results.wait_for_feedback_results().items():
+        
+        meta=feedback_result.calls[0].meta
+
+        if 'reasons' in meta:
+            main_reason = meta['reasons']
+        elif 'reason' in meta:
+            main_reason = meta['reason']
+        else:
+            main_reason = response.args['response']
+            
+        if feedback.name == "custom_metric_score":
+            st.write(“Custom Metric Score”)
+            st.text(f"Answer Relevance: {feedback_result.result}")
+            st.markdown(f"Reason: {main_reason}")
+            st.divider()
         
     # st.write("Answer: ", ans)
     # st.write("Question: ", ques)
